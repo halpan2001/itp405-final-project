468c070ec4e463d8ed8e54b309603f32c52c4cb9
halpan2001 <halpan@usc.edu>
2019-04-29 22:46:46 -0700

    migration for commissions, users_commissions, and purchases table

---
+++ database/migrations/2019_04_30_051500_create_commissions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commissions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->integer('price');
+            $table->integer('slots');
+            $table->boolean('paid');
+            $table->integer('workTime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commissions');
+    }
+}
